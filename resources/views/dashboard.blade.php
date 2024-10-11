43db47b64894035d2c03a8ef4f2d87d6fb530211
Anne Wright <anne.wright@iadt.ie>
2024-10-11 16:05:02 +0100

    Added a Logo to the Dashboard and moved View All Books so it is now a tab - implemented in navigation.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,17 +9,18 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <img src="/path-to-your-logo.png" alt="Logo" class="w-16 h-16 mx-auto">
+                    <img src={{asset( "images/books/logo.png")}} alt="Logo" class="w-16 h-16 mx-auto">
                     <h1 class="text-center text-2xl font-bold mt-4">Welcome to the Book Review App!</h1>
                     <p class="text-center">Explore and review your favorite books!</p>
 
                     <div class="mt-6 text-center">
-                        <a href="{{ route('books.index') }}" class="text-blue-600 underline">
-                            View All Books
-                        </a>
+                            Latest Books (not yet implemented)
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+
+
