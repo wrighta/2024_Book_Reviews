01abe46a5ee81c2d11d02fdc31627256eae775de
Anne Wright <anne.wright@iadt.ie>
2024-11-11 21:39:04 +0000

    Altered AdminSeeder so it creates a new Admin User

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,19 +2,22 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    // In database/seeders/AdminSeeder.php
-    public function run() {
-        \App\Models\User::first()->update([
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin' . time() . '@example.com', // Faking a unique email using timestamp
+            'password' => Hash::make('password'), // Password is hashed for security.
             'role' => 'admin',
         ]);
     }
-
 }
